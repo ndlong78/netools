7130c3664baf54dfdb8c1fc036ab88e502200dd6
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2024-12-25 13:34:43 +0700

    1

--- app/app.py
+++ app/app.py
@@ -1,15 +1,25 @@
+# app/app.py
+from flask import Flask, render_template, request, redirect, url_for
 from app import app
 import tracert
 import database
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    host = 'google.com'  # Bạn có thể thay đổi host theo nhu cầu
-    tracert_output = tracert.tracert_host(host)
-    hops = tracert.parse_tracert(tracert_output)
-    database.save_to_db(host, hops)
+    if request.method == 'POST':
+        host = request.form['host']
+        if host:
+            tracert_output = tracert.tracert_host(host)
+            hops = tracert.parse_tracert(tracert_output)
+            database.save_to_db(host, hops)
+            return redirect(url_for('results', host=host))
+    return render_template('index.html')
+
+@app.route('/results')
+def results():
+    host = request.args.get('host')
     results = database.get_tracert_results(host)
-    return render_template('index.html', host=host, results=results)
+    return render_template('results.html', host=host, results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
