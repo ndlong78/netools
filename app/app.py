bbdb7c28c3bc78eeab42a2e91c0e44e53f9a1ae8
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2024-12-25 15:51:03 +0700

    1

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
-from flask import render_template, request, redirect, url_for, Response
+from flask import render_template, request, redirect, url_for, Response, stream_with_context
 from app import tracert
 from app import database
 import platform
 import subprocess
 import re
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         host = request.form['host']
@@ -12,6 +15,7 @@ def index():
             return redirect(url_for('tracert_progress', host=host))
     return render_template('index.html')
 
+@app.route('/tracert_progress/<host>')
 def tracert_progress(host):
     def generate():
         command = ['traceroute', host] if platform.system() != 'Windows' else ['tracert', host]
@@ -30,15 +34,20 @@ def tracert_progress(host):
 
         process.stdout.close()
         process.wait()
-        database.save_to_db(host, hops)
+        database.save_to_db(host, hops)  # Lưu kết quả vào cơ sở dữ liệu
 
-    return Response(generate(), mimetype='text/event-stream')
+    return Response(stream_with_context(generate()), mimetype='text/event-stream')
 
+@app.route('/results')
 def results():
     host = request.args.get('host')
     results = database.get_tracert_results(host)
     return render_template('results.html', host=host, results=results)
 
+@app.route('/saved_results')
 def saved_results():
     hosts = database.get_all_hosts()
     return render_template('saved_results.html', hosts=hosts)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
